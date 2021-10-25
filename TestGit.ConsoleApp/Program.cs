2ad639b1dc17711f522633ea5e200bc7049ab32a
andrearealcomm <andrea.zanetti@realcomm.it>
2021-10-25 15:28:15 +0200

    Aggiungere i file di progetto.

---
+++ TestGit.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestGit.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Andrea");
+        }
+    }
+}
